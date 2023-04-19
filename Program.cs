3d42e25a0276139a6c3794773d1b9026119ecb9b
Shnitkov <shnitkov2014@mail.ru>
2023-04-19 23:28:04 +0300

    Задача 10

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число:");
+int a = int.Parse(Console.ReadLine());
+int b = a / 10;
+Console.WriteLine(b);
+int c = b % 10;
+Console.WriteLine(c);
+Console.WriteLine("" + c);
+
